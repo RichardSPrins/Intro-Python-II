050b37eae478c0af59d0e3b9ee8766ca90d911fe
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-10-30 16:17:22 -0400

    defined Room and Player classes

--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+  def __init__(self, name, desc):
+    self.name = name
+    self.desc = desc
+  def __str__(self):
+    return f"Room: {self.name}. Details: {self.desc}"
+  # def __repr__(self):
+  #   return f"Room({repr(self.name)})"
+
+newRoom = Room('first room', 'this is the first room')
+
+
+print(newRoom)
